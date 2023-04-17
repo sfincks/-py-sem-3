8277064ce368b68e0b84651f36a74ed575d32282
sfincks <sfincks666@gmail.com>
2023-04-17 16:22:40 +0500

    task 3

--- task3.py
+++ task3.py
@@ -9,4 +9,17 @@
 
 # ноутбук
 #     12
+import re
 
+lstEng = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP',
+          4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+
+lstRus = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ',
+          3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+
+word = input('Введите слово: ').upper()
+
+if bool(re.search('[а-яА-Я]', word)):
+    print(sum([k for i in word for k, v in lstRus.items() if i in v]))
+else:
+    print(sum([k for i in word for k, v in lstEng.items() if i in v]))
